f61b1dff9140980b6e927c39378064f5a3010e15
Łukasz Uczkiewicz <lukasz.uczkiewicz@microsoft.wsei.edu.pl>
2019-09-09 11:36:41 +0200

    Add game grid and pages

--- PuzzleGame/MainPage.xaml.cs
+++ PuzzleGame/MainPage.xaml.cs
@@ -15,5 +15,25 @@ namespace PuzzleGame
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked_Easy(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GamePage());
+        }
+
+        private async void Button_Clicked_Medium(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GamePage());
+        }
+
+        private async void Button_Clicked_Hard(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GamePage());
+        }
+
+        private async void Button_Clicked_Records(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RecordsPage());
+        }
     }
 }
