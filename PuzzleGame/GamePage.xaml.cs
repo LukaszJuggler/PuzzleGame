09a6bc7006e4687d886f33832323b75949182914
Łukasz Uczkiewicz <lukasz.uczkiewicz@microsoft.wsei.edu.pl>
2019-09-11 18:01:22 +0200

    shuffle puzzles

--- PuzzleGame/GamePage.xaml.cs
+++ PuzzleGame/GamePage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿
+using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Image = Xamarin.Forms.Image;
@@ -25,6 +27,10 @@ namespace PuzzleGame
         {
             Image[] xamlElements = new[] { _01, _02, _03, _04, _05, _06, _07, _08, _09 };
 
+            //mix puzzles
+            Random rnd = new Random();
+            int[] order = Enumerable.Range(0, 9).OrderBy(c => rnd.Next()).ToArray();
+
             var tapGestureRecognizer = new TapGestureRecognizer();
             tapGestureRecognizer.Tapped += (sender, e) =>
             {
@@ -32,7 +38,13 @@ namespace PuzzleGame
                 Image theImage = (Image)sender;
 
                 // now you have a reference to the image
-                theImage.Source = xamlElements[0].Source;
+                if (theImage == xamlElements[0] || theImage == xamlElements[1] || theImage == xamlElements[2])
+                {
+                    theImage.Source = xamlElements[8].Source;
+                } else
+                {
+                    theImage.Source = xamlElements[0].Source;
+                }
             };
 
 
@@ -41,7 +53,7 @@ namespace PuzzleGame
             {
                 for (int j = 0; j < numberOfRows; j++)
                 {
-                    xamlElements[count].Source = ImageSource.FromResource($"PuzzleGame.Resource.img01.0{count+1}.jpg");
+                    xamlElements[count].Source = ImageSource.FromResource($"PuzzleGame.Resource.img01.0{order[count]}.jpg");
 
                     xamlElements[count].GestureRecognizers.Add(tapGestureRecognizer);
                     count++;
