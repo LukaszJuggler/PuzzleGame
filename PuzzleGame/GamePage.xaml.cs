e251a9ebc775411ac34412f2c97327785c545aec
Łukasz Uczkiewicz <lukasz.uczkiewicz@microsoft.wsei.edu.pl>
2019-09-11 19:52:59 +0200

    fix shuffle

--- PuzzleGame/GamePage.xaml.cs
+++ PuzzleGame/GamePage.xaml.cs
@@ -26,24 +26,24 @@ namespace PuzzleGame
         private void CreateImages(int numberOfRows)
         {
             Image[] xamlElements = new[] { _01, _02, _03, _04, _05, _06, _07, _08, _09 };
+            int blankPuzzlePos = 0;
+            int moves = 0;
 
             //mix puzzles
             Random rnd = new Random();
-            int[] order = Enumerable.Range(0, 9).OrderBy(c => rnd.Next()).ToArray();
+            int[] order = Enumerable.Range(1, 9).OrderBy(c => rnd.Next()).ToArray();
 
+            //react to taps
             var tapGestureRecognizer = new TapGestureRecognizer();
             tapGestureRecognizer.Tapped += (sender, e) =>
             {
                 // cast to an image
                 Image theImage = (Image)sender;
 
-                // now you have a reference to the image
-                if (theImage == xamlElements[0] || theImage == xamlElements[1] || theImage == xamlElements[2])
-                {
-                    theImage.Source = xamlElements[8].Source;
-                } else
+                if (theImage != xamlElements[blankPuzzlePos])
                 {
                     theImage.Source = xamlElements[0].Source;
+                    moves++;
                 }
             };
 
@@ -53,6 +53,10 @@ namespace PuzzleGame
             {
                 for (int j = 0; j < numberOfRows; j++)
                 {
+                    if (order[count] == 9)
+                    {
+                        blankPuzzlePos = count;
+                    }
                     xamlElements[count].Source = ImageSource.FromResource($"PuzzleGame.Resource.img01.0{order[count]}.jpg");
 
                     xamlElements[count].GestureRecognizers.Add(tapGestureRecognizer);
